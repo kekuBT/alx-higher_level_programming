186b036b809ba20a8309b8922452aabdc476f711
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-07-09 20:12:16 +0300

    0x0C

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+class Base:
+    """ first class called Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class constructor """
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = self.__nb_objects
