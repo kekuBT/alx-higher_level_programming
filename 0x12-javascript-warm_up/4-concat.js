74c80c5a4727efbbb689d8512e4a09d41c9f2b97
root <root@DESKTOP-BRJVJTM.localdomain>
2022-09-06 13:02:45 +0300

    edit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 // script that prints two arguments passed to it, in the following format: “is”
-const args = process.argv.slice(2);
-if (!args[0]) {
-  console.log(args[0] + ' is ' + args[1]);
-} else if (args.length === 1) {
-  console.log(args[0] + ' is ' + args[1]);
-} else {
-  console.log(args[0] + ' is ' + args[1]);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
+
