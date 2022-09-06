07e035c7b843161d967cee8241983ae3b31aa263
root <root@DESKTOP-BRJVJTM.localdomain>
2022-09-06 11:23:35 +0300

    semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,7 @@
 const count = process.argv.slice(2);
 
 if (!count[0]) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(count[0]);
+  console.log(count[0]);
 }
-
-
