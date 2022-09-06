4b2000fad98d8fcb74edf544e1db704bc13a031f
root <root@DESKTOP-BRJVJTM.localdomain>
2022-09-06 11:17:10 +0300

    edit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const count = process.argv;
+const count = process.argv.slice(2);
 
-if (count === null) {
+if (!count[0]) {
     console.log('No argument');
 } else {
     console.log(count[0]);
 }
+
 
