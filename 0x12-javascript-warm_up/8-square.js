8360d979578412062bffba6bb3494abeb3fe3e96
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-09-06 14:36:42 +0300

    add

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// script that prints x times “C is fun”
+// script that prints a square
 const arg = process.argv.slice(2);
 const argInt = parseInt(arg[0]);
 if (isNaN(argInt)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < argInt; i++) {
-    console.log('C is fun');
+    console.log('X'.repeat(argInt));
   }
 }
