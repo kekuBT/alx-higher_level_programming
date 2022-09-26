37cb767299dcb7e10bf5fae99af77446c224f24a
root <root@DESKTOP-BRJVJTM.localdomain>
2022-09-26 20:35:34 +0300

    edit

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -10,6 +10,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
