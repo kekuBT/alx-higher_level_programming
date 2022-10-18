8088416ce8d3a49bdb6b354e1f800600e6290a12
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-10-18 13:21:16 +0300

    edit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 The first argument is the file path
 The content of the file must be read in utf-8
 If an error occurred during the reading, print the error object */
-const arg = process.argv;
 const fs = require('fs');
 
-fs.readFile(arg[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
     if (err) {
         console.log(err);
     } else {
-        console.log(data.toString());
+        process.stdout.write(data);
     }
 });
